69e9297b5e5fe42f3caa234e207a72cd6030eee7
Yu, Subin <soobin45696@gmail.com>
2022-09-26 15:34:43 +0900

    Main.java 수정 #2

--- src/d201914105/Main.java
+++ src/d201914105/Main.java
@@ -1,8 +1,11 @@
 package d201914105;
 
 public class Main {
-	public static void main(String[] args) {
-		String s = "hello world";
+	static void say(String s) {
 		System.out.println(s);
+	}
+
+	public static void main(String[] args) {
+		say("hello world");
 	}
 }
